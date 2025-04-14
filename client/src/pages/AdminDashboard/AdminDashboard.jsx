35a1381ef44f0b14f21bd9e11410a4d35d705194
slavikovab <barbora.slavikova@student.spsmb.cz>
2025-04-14 15:49:40 +0200

    Desing adjustments

--- client/src/pages/AdminDashboard/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,15 +1,36 @@
-import AdminBar from "../../components/AdminBar/AdminBar"
-import dashboardStyles from "../../scss/AdminDashboard.module.scss"
+import AdminBar from "../../components/AdminBar/AdminBar";
+import dashboardStyles from "../../scss/AdminDashboard.module.scss";
+import { useAuth } from "../../context/AuthProvider";
 
 export default function AdminDashboard() {
+  const { user } = useAuth();
   return (
     <>
       <div className={dashboardStyles.page}>
         <AdminBar />
-        <div className={dashboardStyles.dashboard}>
-          <h1>nejaky crazy analytics</h1>
+        <div className={dashboardStyles.edit}>
+          <div className={dashboardStyles.bg}>
+            <div className={dashboardStyles.container}>
+              <div className={dashboardStyles.greeting}>
+                <h1>
+                  Welcome, {user.firstName} {user.lastName}! <br /> Time to
+                  conquer today’s challenges!
+                </h1>
+              </div>
+              <div className={dashboardStyles.statistics}>
+                <div className={dashboardStyles.statistic}>
+                  <h3>REGISTERED USERS</h3>
+                  <p>58</p>
+                </div>
+                <div className={dashboardStyles.statistic}>
+                  <h3>PUBLISHED POSTS</h3>
+                  <p>63</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
