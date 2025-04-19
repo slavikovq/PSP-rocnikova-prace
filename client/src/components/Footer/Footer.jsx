093a25a4ff13d835d4a2b8de221d09bb4303ffc5
slavikovab <barbora.slavikova@student.spsmb.cz>
2025-04-19 23:12:56 +0200

    Design - footer adjustments

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,24 +4,40 @@ import CBIC from "../../assets/icons/CBIC.png"
 
 export default function Footer() {
   return (
-    <>
-      <div className={footerStyles.footer}>
-        <div className={footerStyles.footerBox}>            
-            <div>
-                <p>náměstí Míru 30</p>
-                <p>Mělník</p>
-                <p>276 01</p>
-                <p>Czechia</p>
-            </div>
-            <div>
-                <img src={linkedin} alt="" id={footerStyles.linkedin}/>
-                <p>© 2025 PSP Technologies</p>
-            </div>
-            <div>
-                <img src={CBIC} alt="" id={footerStyles.CBIC} />
-            </div>
+    <footer className={footerStyles.footer}>
+      <div className={footerStyles.footerInner}>
+        <div className={footerStyles.footerBox}>
+          <div className={footerStyles.address}>
+            <p>náměstí Míru 30</p>
+            <p>Mělník</p>
+            <p>276 01</p>
+            <p>Czechia</p>
+          </div>
+
+          <div className={footerStyles.centerBox}>
+            <a
+              href="https://cz.linkedin.com/company/psp-technologies-com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <img src={linkedin} alt="LinkedIn - PSP Technologies" className={footerStyles.linkedin} />
+            </a>
+            <p>© 2025 PSP Technologies</p>
+          </div>
+
+          <div className={footerStyles.badgeBox}>
+            <a
+              href="https://s-ic.cz/en/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="CBIC Certification"
+            >
+              <img src={CBIC} alt="CBIC Certification" className={footerStyles.CBIC} />
+            </a>
+          </div>
         </div>
       </div>
-    </>
+    </footer>
   )
 }
